b52cdfc820a482baa240ac8113de6d2ae824907f
julian <julian94.-@hotmail.com>
2018-06-03 03:37:05 -0300

    Se corrigieron errores en el checkSum, se implemento un ejemplo de el envio con exito por el puerto serie

--- Sistema Embebidos/Raspberry/src/PorteroInteligenteRasp.cpp
+++ Sistema Embebidos/Raspberry/src/PorteroInteligenteRasp.cpp
@@ -7,9 +7,27 @@
 //============================================================================
 
 #include <iostream>
-using namespace std;
+#include <serie/serie.h>
+#include <common/common.h>
 
+#include <wiringSerial.h>
+using namespace std;
+void procesarDato(void* vec, int cant);
 int main() {
-	cout << "!!!Hello soa" << endl; // prints !!!Hello World!!!
+	serie myserie;
+	int fd=myserie.init(procesarDato);
+	char vec[]={'H','o','l','a','J','u','u'};
+
+	myserie.prepare_pack(vec,7);
+while (true){
+	cout << "!!!Hello soa" << fd<<" "<<endl; // prints !!!Hello World!!!
+	usleep(4000000);
+
+}
+	cout << "!!!Hello soa" << fd<<" "<<endl; // prints !!!Hello World!!!
 	return 0;
+}
+
+void procesarDato(void* vec, int cant){
+	cout << "Hello SOAAA!!!" << endl;
 }
