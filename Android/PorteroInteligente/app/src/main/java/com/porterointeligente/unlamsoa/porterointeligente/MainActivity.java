5c362895b9521e0c7ee647f5f5d5c201ebea2f6e
julian <julian94.-@hotmail.com>
2018-06-28 16:31:18 -0300

    - Se implementó el uso de sensores de proximidad. - Se creo label de prueba en la interfaz, para visualizar valores de sensores. - Se agregó en el manifest el sensor de proximidad. - Se Agregó shake para mostrar y ocultar video.

--- Android/PorteroInteligente/app/src/main/java/com/porterointeligente/unlamsoa/porterointeligente/MainActivity.java
+++ Android/PorteroInteligente/app/src/main/java/com/porterointeligente/unlamsoa/porterointeligente/MainActivity.java
@@ -1,6 +1,11 @@
 package com.porterointeligente.unlamsoa.porterointeligente;
 
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,17 +14,23 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
     Button btnVideo;
     Button btnAudio;
     WebView ViewVideo;
+    TextView textVieww;
     String url="http://192.168.10.178:8080/?action=stream";
     Boolean videoActivo=true;
     Boolean microfonoActivo=false;
+    private SensorManager      sensor;
+    private final static float ACC = 30;
+
 //    File file = new File(context.getFilesDir(), filename);
 
     @Override
@@ -27,10 +38,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Log.i("Ejecuto","onCreate.");
         setContentView(R.layout.activity_main);
-
+        sensor = (SensorManager) getSystemService(SENSOR_SERVICE);
         btnVideo=(Button) findViewById(R.id.btnVideo);
         btnAudio=(Button) findViewById(R.id.btnAudio);
         ViewVideo=(WebView) findViewById(R.id.viewVideo);
+        textVieww=(TextView) findViewById(R.id.textView2);
         ViewVideo.setWebViewClient(new WebViewClient());
         WebSettings settings= ViewVideo.getSettings();
         settings.setSupportZoom(true);
@@ -39,6 +51,18 @@ public class MainActivity extends AppCompatActivity {
 //        settings.setLoadsImagesAutomatically(true);
         cargarVideo(null);
 
+        sensor = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        boolean done;
+        done = sensor.registerListener(this, sensor.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+        sensor.registerListener(this,sensor.getDefaultSensor(Sensor.TYPE_PROXIMITY), SensorManager.SENSOR_DELAY_NORMAL);
+        sensor.registerListener(this,sensor.getDefaultSensor(Sensor.TYPE_LIGHT), SensorManager.SENSOR_DELAY_NORMAL);
+        if (!done)
+        {
+            Toast.makeText(this, "No soporta Shake", Toast.LENGTH_SHORT).show();
+
+        }
+        Toast.makeText(this, "Soporta Shake", Toast.LENGTH_SHORT).show();
     }
 
     public void config(View view){
@@ -54,6 +78,14 @@ public class MainActivity extends AppCompatActivity {
 //            Toast.makeText(this,url,Toast.LENGTH_SHORT).show();
         }
     }
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+
+
+    }
+
 
     public void cargarVideo(View view){
         if(videoActivo==true){
@@ -88,4 +120,40 @@ public class MainActivity extends AppCompatActivity {
 //        resu.setText(estado.getString("resultado"));
         ViewVideo.loadUrl(url);
     }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        int sensorType = event.sensor.getType();
+
+        float[] values = event.values;
+
+        if (sensorType == Sensor.TYPE_ACCELEROMETER)
+        {
+            //es Shake?
+            if ((Math.abs(values[0]) > ACC || Math.abs(values[1]) > ACC || Math.abs(values[2]) > ACC))
+            {
+                //Accion al hacer Shake.
+                cargarVideo(null);
+            }
+
+            if(values[2]<-8.7 ){
+                //esta boca abajo
+                textVieww.setText("Apago"+values[0]+"   "+values[1]+"   "+values[2]+"   ");
+            }
+        }else if(sensorType == Sensor.TYPE_PROXIMITY){
+            //0=hay objeto,  5 no hay objeto.
+            if(event.values[0]==0){
+                textVieww.setText(""+event.values[0]);
+            }
+
+        }else if(sensorType == Sensor.TYPE_LIGHT){
+            if(event.values[0]==0){
+                textVieww.setText("Sin luz"+event.values[0]);
+            }
+
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {    }
 }
--- Android/PorteroInteligente/app/src/main/java/com/porterointeligente/unlamsoa/porterointeligente/ServicioVideo.java
+++ Android/PorteroInteligente/app/src/main/java/com/porterointeligente/unlamsoa/porterointeligente/ServicioVideo.java
@@ -8,15 +8,20 @@ import android.support.annotation.Nullable;
 class ServicioVideo extends Service {
     @Nullable
     @Override
+    //Se invoca a este metodo cuando ejecuta bindService
     public IBinder onBind(Intent intent) {
+
         return null;
     }
 
     @Override
     public void onCreate(){
         super.onCreate();
+
     }
 
+    //Despues de onCreate automaticamente se ejecuta a OnstartCommand,
+    //idArranque: Es el id del servicio a ejecutar
     @Override
     public int onStartCommand(Intent intenc, int flags, int idArranque){
     return 1;
