5c9e777052668057115f823f23332ab00d565215
Leonardo <leonardopastor1984@gmail.com>
2018-07-08 23:05:09 -0300

    * Se implentó el módulo encargado de interactuar con los servicios de Firebases:  - el intercambio de información con database firebases se realiza por medio de 2    script (python)  - El módulo se ocupa de determinar que acción tuvo lugar (abrir puerta, cerra puerta, abrir mic) y    ejecutar la correspondiente acción.  - Una vez finalizada la operación se actualiza nuevamente el estado en el servidor de firebases.

--- Sistema Embebidos/Raspberry/src/PorteroInteligenteRasp.cpp
+++ Sistema Embebidos/Raspberry/src/PorteroInteligenteRasp.cpp
@@ -11,15 +11,76 @@
 #include <comunicacion/Comunicacion.h>
 #include <common/common.h>
 #include <sqlite/sqlite.h>
+#include <pthread.h>
+
 using namespace std;
 
 static Comunicacion myserie;
 static sqlite handlerSQlite("acceso.db");
 void eventoMain(int comando, int tam, void *vec);
 
+bool openDoorFirebases = false;
+bool closeDoorFirebases = true;
+bool openMicFirebases = false;
+
+void *firebases(void *arg) {
+
+	FILE *ptr_file;
+	char buffer[100];
+	char oldBuffer[100];
+	char cmd[150];
+	char data[100];
+
+	while (true) {
+		system("python /home/pi/iot/firebases.py > retFirebases.out");
+		ptr_file = fopen("retFirebases.out", "r");
+
+		if (ptr_file) {
+
+			fgets(buffer, sizeof(buffer), ptr_file);
+
+			if (strcmp(buffer, oldBuffer) != 0) {
+				strncpy(oldBuffer, buffer, sizeof(oldBuffer));
+
+				if (buffer[0] == '1') {
+					cout <<"> Firebases: Abrir Puerta" << endl;
+					buffer[0] = '0';
+					snprintf(cmd, sizeof(cmd),"python /home/pi/iot/firebases_sender.py %s", buffer);
+					system(cmd);
+					strncpy(data, "trash", sizeof(data));
+					myserie.enviarDatos(OPEN_DOOR + 48, 2, data);
+				}
+				if (buffer[2] == '1') {
+					cout << "> Firebases: Cerrar Puerta" << endl;
+					buffer[2] = '0';
+					snprintf(cmd, sizeof(cmd),"python /home/pi/iot/firebases_sender.py %s", buffer);
+					system(cmd);
+					strncpy(data, "trash", sizeof(data));
+					myserie.enviarDatos(CLOSE_DOOR + 48, 2, data);
+				}
+				if (buffer[4] == '1') {
+					cout << "> Firebases: abrir mic" << endl;
+					buffer[4] = '0';
+					snprintf(cmd, sizeof(cmd),"python /home/pi/iot/firebases_sender.py %s", buffer);
+					system(cmd);
+				}
+
+				fclose(ptr_file);
+			}
+		}
+		usleep(10000);
+	}
+}
+
 int main() {
 	myserie.init(eventoMain);
 	handlerSQlite.init();
+	pthread_t threadId;
+	int err = pthread_create(&threadId, NULL, firebases, NULL); //eventoSerial es donde se fija si hay un mensaje.
+	if (err != 0) {
+		printf("Error al crear hilo firebases.");
+		return -1;
+	}
 
 	while (true) {
 
